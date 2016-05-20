14de12381911d3406dd5909b48f3229c6250961e
het1709 <het1709@hotmail.com>
2016-05-20 17:56:36 -0400

    Displaying paired devices works perfectly. Discovering new devices produces fatal exception.

--- app/src/main/java/com/personal/het/bluetoothmessage/MainActivity.java
+++ app/src/main/java/com/personal/het/bluetoothmessage/MainActivity.java
@@ -1,12 +1,17 @@
 package com.personal.het.bluetoothmessage;
 
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,28 +20,77 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter bAdapter;
-    private ArrayAdapter<String> bArrayAdapter;
+    private ArrayAdapter<String> bPairedAdapter;
+    private ArrayAdapter<String> bDiscoveredAdapter;
     private ListView lvPairedDevices;
-    private TextView textView;
+    private ListView lvDiscoveredDevices;
+    private EditText txtMessage;
+    private TextView msgNoPaired;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bAdapter = BluetoothAdapter.getDefaultAdapter();
-        bArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        bPairedAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        bDiscoveredAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         lvPairedDevices = (ListView) findViewById(R.id.lvPairedDevices);
-        textView = (TextView) findViewById(R.id.textView);
-
+        lvDiscoveredDevices = (ListView) findViewById(R.id.lvDiscoveredDevices);
+        msgNoPaired = (TextView) findViewById(R.id.msgNoPaired);
+        txtMessage = (EditText) findViewById(R.id.txtMessage);
+        txtMessage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                txtMessage.setText("");
+                return false;
+            }
+        });
+        checkBTStatus();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bReceiver, filter);
+        //getPairedDevices();
     }
+
+    private final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            //When discovery finds a device
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                //Get BluetoothDevice from Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                bDiscoveredAdapter.add(device.getName() + "\n" + device.getAddress());
+                bDiscoveredAdapter.notifyDataSetChanged();
+            }
+            lvDiscoveredDevices.setAdapter(bDiscoveredAdapter);
+        }
+    };
+
 
     public void onStart(){
         super.onStart();
-        checkBTStatus();
+        msgNoPaired.setVisibility(View.INVISIBLE);
+        bPairedAdapter.clear();
+        bDiscoveredAdapter.clear();
+        bPairedAdapter.notifyDataSetChanged();
+    }
+
+    public void onResume(){
+        super.onResume();
+        msgNoPaired.setVisibility(View.INVISIBLE);
         getPairedDevices();
     }
 
-    //Checks is the device supports Bluetooth and enables it
+    public void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(bReceiver);
+    }
+
+    public void startDiscovery(){
+        bAdapter.startDiscovery();
+    }
+
+    //Checks if the device supports Bluetooth and enables it
     public void checkBTStatus(){
         if(bAdapter == null){
             Context context = getApplicationContext();
@@ -52,15 +106,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Gets the Paired Devices
     public void getPairedDevices(){
         Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
         if(pairedDevices.size()>0) {
             for (BluetoothDevice device : pairedDevices) {
-                bArrayAdapter.add(device.getName() + "/n" + device.getAddress());
+                bPairedAdapter.add(device.getName() + "\n" + device.getAddress());
             }
-            lvPairedDevices.setAdapter(bArrayAdapter);
+            lvPairedDevices.setAdapter(bPairedAdapter);
         } else {
-            textView.setText(R.string.noPairedDevices);
+            msgNoPaired.setVisibility(View.VISIBLE);
         }
     }
 }
